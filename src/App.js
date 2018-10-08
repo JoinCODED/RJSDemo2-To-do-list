0ba2bd4bd91155a46ec4a8cdd0928675bf840d0f
Octowl <alsaff1987@gmail.com>
2018-10-08 18:07:20 +0300

    Reset to html

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import React, { Component } from "react";
 
 import "../node_modules/font-awesome/css/font-awesome.min.css";
-
 import "./App.css";
-import tasks from "./data";
 
 class App extends Component {
   render() {
-    return (
-      <div class="rectangle">
-        <p class="title">TO DO LIST</p>
-        <table class="table">
-          <tr class="headerRow">
-            <th>STATUS</th>
-            <th>TASK</th>
-            <th>PRIORITY</th>
-          </tr>
-          <tr class="row">
-            <td>
-              <i className="fa fa-check-circle" />
-            </td>
-            <td>Go to the supermarket</td>
-            <td className="middle">MIDDLE</td>
-          </tr>
-          <tr class="row">
-            <i className="fa fa-times-circle" />
-            <td>Cook dinner</td>
-            <td className="high">HIGH</td>
-          </tr>
-          <tr class="row">
-            <i className="fa fa-times-circle" />
-            <td>Call Mom</td>
-            <td className="low">LOW</td>
-          </tr>
-        </table>
-      </div>
-    );
+    return <div />;
   }
 }
 
